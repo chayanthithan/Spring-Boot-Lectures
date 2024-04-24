d75a80b7a137066f75fd9aa6a7a4ba7fc881f6d6
chayanthithan <kumarchayan31@gmail.com>
2024-04-24 17:46:55 +0530

    postgreSql example

--- Day07/StudentManagement/src/main/java/com/sonchayan/StudentManagement/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.sonchayan.StudentManagement.service;
-
-import com.sonchayan.StudentManagement.model.RequestModel;
-
-public interface StudentService {
-    Boolean storeRegisteredStudentsDetails(RequestModel requestModel);
-
-    boolean checkStudent(String studentEmail, String studentPassword);
-}
